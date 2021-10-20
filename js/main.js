ab0d0dbf6cd3bb7a812fd17607b908d07988932e
Vu Khanh Nguyen <vukhanh.nguyen@citynow.vn>
2021-10-20 14:40:10 +0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*  ---------------------------------------------------
+    Template Name: Sona
+    Description: Sona Hotel Html Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    //Offcanvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+
+
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+
+
+
+})(jQuery);
